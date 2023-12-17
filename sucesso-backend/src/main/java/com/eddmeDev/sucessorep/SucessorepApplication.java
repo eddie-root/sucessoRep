f005baa609813467717af5b52830cd8e3a7052d6
Admilson Ferreira <ferreiraad3@gmail.com>
2023-12-17 07:32:37 -0400

    first commit

---
+++ sucesso-backend/src/main/java/com/eddmeDev/sucessorep/SucessorepApplication.java
@@ -0,0 +1,13 @@
+package com.eddmeDev.sucessorep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucessorepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SucessorepApplication.class, args);
+	}
+
+}
---
+++ sucesso-backend/src/test/java/com/eddmeDev/sucessorep/SucessorepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddmeDev.sucessorep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucessorepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
