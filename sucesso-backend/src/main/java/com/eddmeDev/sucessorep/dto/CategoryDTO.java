ecacc369555f3db3ed1d4a425ab38f83faef4761
Admilson Ferreira <ferreiraad3@gmail.com>
2023-12-26 10:35:11 -0400

    Category DTO e Repository

---
+++ sucesso-backend/src/main/java/com/eddmeDev/sucessorep/dto/CategoryDTO.java
@@ -0,0 +1,42 @@
+package com.eddmeDev.sucessorep.dto;
+
+import com.eddmeDev.sucessorep.entities.Category;
+
+public class CategoryDTO {
+
+	private Long id;
+	private String name;
+	
+	public CategoryDTO() {
+	}
+	
+	public CategoryDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public CategoryDTO(Category entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
