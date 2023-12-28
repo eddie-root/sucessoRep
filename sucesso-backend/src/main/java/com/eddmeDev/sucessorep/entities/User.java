e70f568652e9d258730aa7cb2412341f478e131d
Admilson Ferreira <ferreiraad3@gmail.com>
2023-12-28 00:44:12 -0400

    UserDTO, RoleDTO

--- sucesso-backend/src/main/java/com/eddmeDev/sucessorep/entities/User.java
+++ sucesso-backend/src/main/java/com/eddmeDev/sucessorep/entities/User.java
@@ -1,11 +1,17 @@
 package com.eddmeDev.sucessorep.entities;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +24,12 @@ public class User {
 	private String name;
 	private String email;
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 	
 	public User() {
 	}
@@ -60,6 +72,11 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
 
 	@Override
 	public int hashCode() {
